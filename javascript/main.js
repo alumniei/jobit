c9f7974f79f1edb23628ba1746df338f83cbe818
Sofia Reis <6732277+sofiaoreis@users.noreply.github.com>
2018-02-23 23:56:57 +0000

    Revert "jobit18"

--- javascript/main.js
+++ javascript/main.js
@@ -1,75 +1,12 @@
-function consoleText(words, id, colors) {
-    if (colors === undefined) colors = ['#fff'];
-    var visible = true;
-    var con = document.getElementById('console');
-    var letterCount = 1;
-    var x = 1;
-    var waiting = false;
-    var stop = false;
-    var target = document.getElementById(id)
-    target.setAttribute('style', 'color:' + colors[0])
-    window.setInterval(function() {
-
-        if (letterCount === 0 && waiting === false) {
-            waiting = true;
-            target.innerHTML = words[0].substring(0, letterCount)
-            window.setTimeout(function() {
-                var usedColor = colors.shift();
-                colors.push(usedColor);
-                var usedWord = words.shift();
-                words.push(usedWord);
-                x = 1;
-                target.setAttribute('style', 'color:' + colors[0])
-                letterCount += x;
-                waiting = false;
-            }, 1000)
-        } else if (letterCount === words[0].length + 1 && waiting === false) {
-            waiting = true;
-            stop = true;
-            window.setTimeout(function() {
-                x = -1;
-                letterCount += x;
-                waiting = false;
-            }, 1000)
-        } else if (waiting === false) {
-            target.innerHTML = words[0].substring(0, letterCount)
-            letterCount += x;
-        }
-    }, 120)
-    var us = window.setInterval(function() {
-       /* if (stop) {
-            con.className = 'console-underscore hidden';
-            clearInterval(us);
-        } else {*/
-            if (visible === true) {
-                con.className = 'console-underscore hidden'
-                visible = false;
-
-            } else {
-                con.className = 'console-underscore'
-
-                visible = true;
-            }
-        //}
-    }, 400)
-}
-
 jQuery(document).ready(function($){
-    consoleText(['4th, 11th and 18th April'], 'text', ['white']);
+
 	$( ".round-img" ).hover(function() {
 		$( this ).parent().parent().find("p").css('opacity',1.0);
 	});
 
-    $( ".round-img" ).mouseleave(function() {
-        $( this ).parent().parent().find("p").css('opacity',0.0);
-    });
-    
-    $(function() {
-      $('#scroll-down-btn').on('click', function(e) {
-        e.preventDefault();
-        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-      });
-    });
+$( ".round-img" ).mouseleave(function() {
+$( this ).parent().parent().find("p").css('opacity',0.0);
+});
 
 	var transitionEnd = 'webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend';
 	var transitionsSupported = ( $('.csstransitions').length > 0 );
--- previous-editions/2017/javascript/animation.js
+++
@@ -1,7 +0,0 @@
-$( document ).ready(function() {
-$("#Camada_1").attr("class", "animated-logo");
-
-$("#Camada_1").hover(function() {
-    $("#Camada_1").attr("class", "logo");
-  })
-});
